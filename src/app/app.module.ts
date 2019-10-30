d6780cc4d73539412a3bd05bd73d40255db3448d
thor1824 <42994659+thor1824@users.noreply.github.com>
2019-10-30 02:41:11 +0100

    w

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 import {ConfirmationDialogComponent} from './components/shared/confirmation-dialog/confirmation-dialog.component';
-import {MatNativeDateModule} from '@angular/material/core';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
@@ -33,20 +33,7 @@ import { HttpClientModule } from '@angular/common/http';
     ConfirmationDialogComponent,
     PetCreateComponent
   ],
-  imports: [BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    MatDialogModule,
-    MatButtonModule,
-    BrowserAnimationsModule,
-    MatNativeDateModule,
-    MatDatepickerModule,
-    MatIconModule,
-    MatInputModule,
-    MatPaginatorModule,
-    ReactiveFormsModule,
-    HttpClientModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, MatDialogModule, MatButtonModule, BrowserAnimationsModule, MatNativeDateModule, MatDatepickerModule, MatIconModule, MatInputModule, MatPaginatorModule, ReactiveFormsModule, HttpClientModule, MatRippleModule],
   entryComponents: [ConfirmationDialogComponent],
   providers: [],
   bootstrap: [AppComponent]
